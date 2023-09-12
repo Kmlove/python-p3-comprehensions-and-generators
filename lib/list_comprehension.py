651c02eadf91b31a4098dd381268b422cd63249c
Kim Love <kimberlymlove15@gmail.com>
2023-09-12 17:14:19 -0400

    All tests passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,13 @@
 
 def return_evens(num_list):
     pass
+    # returnValue iterator conditional
+    # evenElements lengthOfList ifNumIsEven
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens 
 
 def make_exclamation(sentence_list):
     pass
+    # returnValue iterator
+    exclamation = [string + "!" for string in sentence_list]
+    return exclamation
